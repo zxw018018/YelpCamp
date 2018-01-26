f41826e352ebda2c55fafbb0a96f37dd8e0b596b
Xinwei <zxw018018@hotmail.com>
2018-01-26 16:19:46 -0500

    add dburl

--- app.js
+++ app.js
@@ -17,9 +17,12 @@ var	commentRoutes 		= require("./routes/comments"),
 	campgroundRoutes 	= require("./routes/campgrounds"),
 	indexRoutes 		= require("./routes/index");
 
-// mongoose.connect("mongodb://localhost/yelp_camp_v6");
-mongoose.connect("mongodb://xinwei:xinwei@ds115758.mlab.com:15758/xinwei-yelpcamp");
 
+// set up databaseurl
+// export DATABASEURL=
+mongoose.connect(process.env.DATABASEURL);
+// mongoose.connect("mongodb://localhost/yelp_camp_v6");
+// mongoose.connect("mongodb://xinwei:xinwei@ds115758.mlab.com:15758/xinwei-yelpcamp");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -54,7 +57,7 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments",commentRoutes);
 
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(process.env.PORT||3000, process.env.IP, function(){
 	console.log("Yelp Server has started!");
 });
 
