9e0b3bf7ec52a9b06422d783494b8fff5ef0ef9b
Xinwei <zxw018018@hotmail.com>
2018-01-26 15:04:52 -0500

    add mlab

--- app.js
+++ app.js
@@ -17,7 +17,10 @@ var	commentRoutes 		= require("./routes/comments"),
 	campgroundRoutes 	= require("./routes/campgrounds"),
 	indexRoutes 		= require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp_v6");
+// mongoose.connect("mongodb://localhost/yelp_camp_v6");
+mongoose.connect("mongodb://xinwei:xinwei@ds115758.mlab.com:15758/xinwei-yelpcamp");
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
