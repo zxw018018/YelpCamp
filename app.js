a80b7a194b6874ba524e47c93a58f26869f24b27
Xinwei <zxw018018@hotmail.com>
2018-01-26 14:34:15 -0500

    change port

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments",commentRoutes);
 
 
-app.listen(3000, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
 	console.log("Yelp Server has started!");
 });
 
